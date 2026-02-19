16bab2cd664e5e723230936f600268b09b23453b
Prathamesh Padher <prathamesh.padher27@gmail.com>
2026-02-19 14:22:34 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: "https://event-regwebsite.netlify.app/"
+  origin: "https://event-regwebsite.netlify.app"
 }));
 app.use(express.json());
 
